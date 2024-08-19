ed35124b8c73c38356c19fb281187317f9dc233a
Ducky Duckerson <102842567+DuckyDuckerson@users.noreply.github.com>
2024-08-18 21:30:05 -0700

    update

--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,57 @@ use std::cmp::Ordering;
 
 
 fn main() {
-    guessing_game();
+    let secret_number = r_gen();
+
+    guessing_game(secret_number);
     // -------------------------
     println!("Goodbye!");
 }
 
 
-fn guessing_game(){
-    let secret_number = rand::thread_rng().gen_range(1..=10);
+fn r_gen() -> u32{
+    // Saw this being talked about on reddit so I wrote it in rust   
+
+    let z = 0;
+    let x = 1;
+    let a = 1;
+    let b = 2;
+    let c = 5;
+    let d = 7;
+    let e = 10;
+    let f = 15;
+    let mut i = rand::thread_rng().gen_range(x..=a);
+    let mut y = rand::thread_rng().gen_range(b..=d);
+
+
+    let mut r = rand::thread_rng().gen_range(z..=e);
+    match r {
+        1..=50 => {
+            r = rand::thread_rng().gen_range(i..=y);
+            return r;
+        }
+        51..=100 => {
+            i = rand::thread_rng().gen_range(c..=e);
+            y = rand::thread_rng().gen_range(z..=f);
+            r = rand::thread_rng().gen_range(i..=y);
+            return r;
+        }
+        _ => {
+            i = rand::thread_rng().gen_range(c..=f);
+            y = rand::thread_rng().gen_range(e..=f);
+            r = rand::thread_rng().gen_range(i..=y);
+            return r;
+        }
+    };
+}
+
+
+fn guessing_game(secret_number: u32){
 
     let mut tries = 0;
 
     while tries < 3{
-        println!("Guess the number! Between 1 and 10.");
+        println!("Guess the number! Between 1 and 15.");
         println!("Please input your guess.");
         
         let mut guess = String::new();
